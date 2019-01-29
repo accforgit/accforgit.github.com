d9d79a27db2f72d431899b06e0fecfde36cb6ca4
zhuhui <zhuhui@zhuanzhuan.com>
2019-01-29 16:24:05 +0800

    add flip

---
+++ flip/precache-manifest.713f7c268834584974ab482e89f2ab89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "784a47d0692eac1ac328",
+    "url": "/static/js/main.784a47d0.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "784a47d0692eac1ac328",
+    "url": "/static/css/main.07000192.chunk.css"
+  },
+  {
+    "revision": "229bd60d7738b5af2a0ca54e5d6875da",
+    "url": "/index.html"
+  }
+];
